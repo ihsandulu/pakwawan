5b1e4d8c21f2745de0ae3758e1cddf42be2f6fc0
ihsandulu <ihsan.dulu@gmail.com>
2025-10-06 14:55:10 +0700

    bug export excel master produk

--- app/Views/master/mproduct_v.php
+++ app/Views/master/mproduct_v.php
@@ -238,7 +238,7 @@
                         <?php } ?>
 
                         <div class="table-responsive m-t-40">
-                            <table id="example23" class="display nowrap table table-hover table-striped table-bordered" cellspacing="0" width="100%">
+                            <table id="example230" class="display nowrap table table-hover table-striped table-bordered" cellspacing="0" width="100%">
                                 <!-- <table id="dataTable" class="table table-condensed table-hover w-auto dtable"> -->
                                 <thead class="">
                                     <tr>
